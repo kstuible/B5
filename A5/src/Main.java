62588670d7d06d07df0d571b246a6f61fd0467bc
kevotron9000 <kevotron9000@wireless141-165.wireless.wmich.edu>
2018-10-31 11:09:13 -0400

    Kevin - Wrote second print statement

--- A5/src/Main.java
+++ A5/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I'm partner 1, I wrote this statement");
+		
+		System.out.println("I'm partner 2, I wrote this Statement");
 	}
 
 }
